e7cd59623652f2c7eef2958482a8453b26cc85a9
Alba Gómez Pérez <albagomezperez@MacBook-Pro-de-Alba.local>
2026-01-08 17:29:58 +0100

    add new method to get pet by id

--- src/app/core/services/pets.service.ts
+++ src/app/core/services/pets.service.ts
@@ -1,4 +1,4 @@
-import {Pet} from "../models/pet.model";
+import { Pet } from "../models/pet.model";
 
 const API_URL: string = 'https://my-json-server.typicode.com/Feverup/fever_pets_data/pets';
 
@@ -24,6 +24,27 @@ const petsService = {
         } catch (error) {
             console.error('petsService.getAllPets:', error);
             return [];
+        }
+    },
+
+    /**
+     * Get a pet by id
+     * @param {number} id - The id of the pet
+     * @returns {Promise<Pet | undefined>} The pet found
+     */
+    async getPetById(id: number): Promise<Pet | undefined> {
+        try {
+            const response = await fetch(`${API_URL}/${id}`);
+
+            if (!response.ok) {
+                throw new Error(`Error al obtener el pet: ${response.status}`);
+            }
+
+            const data: Pet = await response.json();
+            return data;
+        } catch (error) {
+            console.error('petsService.getPetById:', error);
+            return undefined;
         }
     },
 };
