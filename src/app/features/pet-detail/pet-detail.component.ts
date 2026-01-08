ea9af29ecbb8fd0b6f4e5de0b5d932ce11aa4d54
Alba Gómez Pérez <albagomezperez@Mac.home>
2026-01-08 10:55:00 +0100

    create a first structure and get pets

---
+++ src/app/features/pet-detail/pet-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'app-pet-detail',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './pet-detail.component.html',
+    styleUrls: ['./pet-detail.component.scss']
+})
+export class PetDetailComponent {
+    // Aquí irán luego las señales y métodos
+}
---
+++ src/app/features/pet-list/pet-list.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import petsService from "../../core/services/pets.service";
+import type { Pet } from '../../core/models/pet.model';
+
+@Component({
+    selector: 'app-pet-list',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './pet-list.component.html',
+    styleUrls: ['./pet-list.component.scss']
+})
+export class PetListComponent implements OnInit {
+    pets: Pet[] = [];
+
+    ngOnInit() {
+        this.getPets();
+    }
+    async getPets() {
+        this.pets = await petsService.getAllPets();
+        console.log('Todas las mascotas:', this.pets);
+    }
+}
