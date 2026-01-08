b24333bcd59d29de6f8847f9f3e4e6c58644cc47
Alba Gómez Pérez <albagomezperez@MacBook-Pro-de-Alba.local>
2026-01-08 17:22:01 +0100

    add structure and funtionality to pet list view

--- src/app/features/pet-list/pet-list.component.ts
+++ src/app/features/pet-list/pet-list.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 import petsService from "../../core/services/pets.service";
 import type { Pet } from '../../core/models/pet.model';
 
@@ -13,11 +14,17 @@ import type { Pet } from '../../core/models/pet.model';
 export class PetListComponent implements OnInit {
     pets: Pet[] = [];
 
+    constructor(private router: Router) { }
+
     ngOnInit() {
         this.getPets();
     }
     async getPets() {
         this.pets = await petsService.getAllPets();
         console.log('Todas las mascotas:', this.pets);
+    }
+
+    goToDetail(id: number) {
+        this.router.navigate(['/pets', id]);
     }
 }
